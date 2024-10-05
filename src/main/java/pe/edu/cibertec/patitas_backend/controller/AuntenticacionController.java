020190f0583b20e387fc44b0a661a2c501af5561
Cristhian Fernando Vasquez Albornoz <cristhianalbornoz9@gmail.com>
2024-10-04 23:42:59 -0500

    Configuracion de sleep por 5 s 04/10/24

--- src/main/java/pe/edu/cibertec/patitas_backend/controller/AuntenticacionController.java
+++ src/main/java/pe/edu/cibertec/patitas_backend/controller/AuntenticacionController.java
@@ -10,6 +10,8 @@ import pe.edu.cibertec.patitas_backend.dto.LoginResponseDTO;
 import pe.edu.cibertec.patitas_backend.service.AutenticacionService;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/autenticacion")
@@ -23,12 +25,14 @@ public class AuntenticacionController {
 
 
         try {
+            Thread.sleep(Duration.ofSeconds(5));
             String[] datosUsuario = autenticacionService.validarUsuario(loginRequestDTO);
+            System.out.println("Resultado: " + Arrays.toString(datosUsuario));
             if (datosUsuario == null){
                 return new LoginResponseDTO("01","Error: Usuario no encontrado","","");
             }
             return new LoginResponseDTO("00","",datosUsuario[0], datosUsuario[1]);
-        } catch (IOException e) {
+        } catch (Exception e) {
             return new LoginResponseDTO("99","Error: Ocurrio un problema","", "");
         }
 
